0c9cde59122322ed5bd8d8117b8416c7b5771f27
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2023-06-03 19:28:22 -0400

    update

--- R/LS_empirical_pvalue.R
+++ R/LS_empirical_pvalue.R
@@ -3,7 +3,12 @@
 
 # Monte Carlo simulation of Lin-Sullivan statistics given covariance and degrees of freedom
 #' @importFrom mvtnorm rmvnorm
-get_stat_samples = function(V, nu = rep(Inf, nrow(V)), n.mc.samples=1e5 ){
+get_stat_samples = function(V, nu = rep(Inf, nrow(V)), n.mc.samples=1e5, seed=1 ){
+
+	# use specified seed internally, then reset to original value
+	old <- .Random.seed
+    on.exit({.Random.seed <<- old})
+    set.seed(seed)
 
 	p = nrow(V)
 
@@ -52,14 +57,14 @@ get_stat_samples = function(V, nu = rep(Inf, nrow(V)), n.mc.samples=1e5 ){
 # @param n.mc.samples number of Monte Carlo samples
 #  
 #' @importFrom MASS fitdistr
-.LS_empirical_pvalue = function(LS.stat, V, nu = rep(Inf, nrow(V)), n.mc.samples=1e5){
+.LS_empirical_pvalue = function(LS.stat, V, nu = rep(Inf, nrow(V)), n.mc.samples=1e5, seed=1){
 
 	stopifnot( nrow(V) == ncol(V) )
 	stopifnot( nrow(V) == length(nu) )
 	stopifnot( all(nu > 1) )
 
 	# sample stats assuming finite sample size
-	stats = get_stat_samples(V, nu, n.mc.samples)
+	stats = get_stat_samples(V, nu, n.mc.samples, seed)
 
 	fit.g = suppressWarnings(fitdistr(stats, "gamma"))
 
@@ -76,6 +81,7 @@ get_stat_samples = function(V, nu = rep(Inf, nrow(V)), n.mc.samples=1e5 ){
 #' @param cor correlation matrix between of test statistics.  Default considers uncorrelated test statistics 
 #' @param nu array of degrees of freedom values, one for each coefficient
 #' @param n.mc.samples number of Monte Carlo samples
+#' @param seed random seed so results are reproducable
 #' 
 #' @details The theorical null for the Lin-Sullivan statistic for fixed effects meta-analysis is chisq when the regression coefficients being tested are normally distributed. This works  asymptotically when the regression coefficients are estimated from a sufficiently large sample.  But when the coefficients are estimated from a small sample, they have a Student-t null distrubtion that is not well approximated by a normal.  In this case, no cumulative distribution function exists in closed form.  Here we simulate coefficients from the student-t under the null given the covariance and degrees of freedom, compute the Lin-Sullivan statistic and then fit a gamma distribution to these null samples.  The p-value for the observed Lin-Sullivan statistic is then computed using this gamma approximation.
 #'
@@ -106,7 +112,7 @@ get_stat_samples = function(V, nu = rep(Inf, nrow(V)), n.mc.samples=1e5 ){
 #' LS.empirical(beta, stders, C, nu)
 #' @export
 #' @seealso \code{LS()}
-LS.empirical = function(beta, stders, cor = diag(1, length(beta)), nu = rep(Inf, length(beta)), n.mc.samples=1e5){
+LS.empirical = function(beta, stders, cor = diag(1, length(beta)), nu = rep(Inf, length(beta)), n.mc.samples=1e5, seed=1){
 
 	# compute Lin-Sullivan test-statistic
 	res = LS(beta, stders, cor)
@@ -115,7 +121,7 @@ LS.empirical = function(beta, stders, cor = diag(1, length(beta)), nu = rep(Inf,
 
 	# Compute empirical p-value using a gamma fit to 
 	# Monte Carlo samples from the null distribution
-	res$p = .LS_empirical_pvalue(with(res, (beta/se)^2), V, nu, n.mc.samples )
+	res$p = .LS_empirical_pvalue(with(res, (beta/se)^2), V, nu, n.mc.samples, seed )
 
 	res
 }
